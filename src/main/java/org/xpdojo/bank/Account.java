1619d5e88919451242f8cbc1b7cde77603609422
shenoy <ragh_shenoy@hotmail.com>
2020-08-06 12:27:33 +0200

    test

--- src/main/java/org/xpdojo/bank/Account.java
+++ src/main/java/org/xpdojo/bank/Account.java
@@ -1,4 +1,16 @@
 package org.xpdojo.bank;
 
 public class Account {
+
+    private int balance = 0;
+     public int balance()
+     {
+         return balance;
+     }
+
+     public void deposit(int amount){
+
+        balance = amount ;
+     }
+
 }
